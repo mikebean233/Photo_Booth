c809f0846e6668c8b180c0247432772bdc80fb11
Michael Peterson <mikebean4551@gmail.com>
2017-07-15 22:31:55 -0700

    refactor

--- Kinect/Experiments/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ Kinect/Experiments/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
 using System.Drawing;
+using System.Security.Permissions;
 using System.Windows.Interop;
 
 namespace WpfApp1
@@ -26,10 +28,7 @@ namespace WpfApp1
 
 	{
 		private KinectSensor sensor;
-		private BitmapSource infraredSource;
-		private BitmapSource depthSource;
-		private BitmapSource visibleSource;
-
+		
 		public MainWindow()
 		{
 			sensor = KinectSensor.GetDefault();
@@ -37,173 +36,185 @@ namespace WpfApp1
 			{
 				MultiSourceFrameReader reader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared);
 				reader.MultiSourceFrameArrived += ReaderOnMultiSourceFrameArrived;
-
-				//sensor.InfraredFrameSource.OpenReader().FrameArrived += OnInfraredArrived;
-				//sensor.DepthFrameSource.OpenReader().FrameArrived += onDepthFrameArrived;
-				//sensor.ColorFrameSource.OpenReader().FrameArrived += OnVisibleFrameArrived;
-				//sensor.Open();
-
-
-
+				sensor.Open();
 				InitializeComponent();
 			}
 		}
 
-		private void ReaderOnMultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs multiSourceFrameArrivedEventArgs)
+		private void ReaderOnMultiSourceFrameArrived(object sender,
+			MultiSourceFrameArrivedEventArgs multiSourceFrameArrivedEventArgs)
 		{
-				
+			if (multiSourceFrameArrivedEventArgs == null)
+				return;
+
+			ColorFrame colorFrame = multiSourceFrameArrivedEventArgs.FrameReference.AcquireFrame().ColorFrameReference
+				.AcquireFrame();
+			InfraredFrame infraredFrame = multiSourceFrameArrivedEventArgs.FrameReference.AcquireFrame().InfraredFrameReference
+				.AcquireFrame();
+			DepthFrame depthFrame = multiSourceFrameArrivedEventArgs.FrameReference.AcquireFrame().DepthFrameReference
+				.AcquireFrame();
+			
+			// Color
+			if (colorFrame != null)
+			{
+				colorPreview.Source = GetBitmapSourceFromFrame(colorFrame);
+				colorFrame.Dispose();
+			}
+			// Infrared
+			if (infraredFrame != null)
+			{
+				infraredPreview.Source = GetBitmapSourceFromFrame(infraredFrame);
+				infraredFrame.Dispose();
+			}
+			
+			// Depth
+			if (depthFrame != null)
+			{
+				depthPreview.Source = GetBitmapSourceFromFrame(depthFrame);
+				depthFrame.Dispose();
+			}
 		}
 
-		private void OnVisibleFrameArrived(object sender, ColorFrameArrivedEventArgs colorFrameArrivedEventArgs)
+		private ImageSource GetBitmapSourceFromFrame(IDisposable frame)
 		{
-			Byte[] array = null;
+			KinectFrame kFrame = new KinectFrame(frame);
 
-			ColorFrame frame = colorFrameArrivedEventArgs.FrameReference.AcquireFrame();
-			if (frame != null)
+			int width = kFrame.FrameDescription().Width;
+			int height = kFrame.FrameDescription().Height;
+			ushort[] shortPixels = new ushort[width * height];
+			byte[] bytePixels = kFrame.getPixelArray();
+
+			if (frame is ColorFrame)
 			{
-				int width = frame.FrameDescription.Width;
-				int height = frame.FrameDescription.Height;
-				PixelFormat format = PixelFormats.Bgr32;
-
-				byte[] pixels = new byte[width * height * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
-
-				if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+				kFrame.CopyRawFrameToArray(bytePixels);
+			}
+			else
+			{
+				kFrame.CopyFrameDataToArray(shortPixels);
+				int outIndex = 0;
+				for (int i = 0; i < shortPixels.Length; ++i)
 				{
-					frame.CopyRawFrameDataToArray(pixels);
+					bytePixels[outIndex++] = (byte) (shortPixels[i] & 0x00ff);
+					bytePixels[outIndex++] = (byte) ((shortPixels[i] & 0xff00) >> 8);
 				}
+			}
+
+			return BitmapSource.Create(width, height, 96, 96, kFrame.PixelFormat() , null, bytePixels, kFrame.Stride());
+		}
+
+		class KinectFrame
+		{
+			private readonly IDisposable _thisFrame;
+			private readonly Collection<Type> _supportedFrames = new Collection<Type>(
+				new Type[]
+				{
+					typeof(ColorFrame),
+					typeof(InfraredFrame),
+					typeof(DepthFrame)
+				}
+			);
+
+			public KinectFrame(IDisposable frame)
+			{
+				if(frame == null)
+					throw new NullReferenceException();
+
+				if(!_supportedFrames.Contains(frame.GetType()))
+					throw new NotSupportedException(frame.GetType() + " is not a supported frame type");
+
+				_thisFrame = frame;
+			}
+
+			public FrameDescription FrameDescription()
+			{
+				if(_thisFrame is ColorFrame)
+					return (_thisFrame as ColorFrame).FrameDescription;
+				else if (_thisFrame is DepthFrame)
+					return (_thisFrame as DepthFrame).FrameDescription;
 				else
-				{
+					return (_thisFrame as InfraredFrame).FrameDescription;
+			}
+
+			public ColorImageFormat RawColorImageFormat()
+			{
+				if (!(_thisFrame is ColorFrame))
+					throw new NotSupportedException("RawColorImageFormat() is not supported for frame of type " + _thisFrame.GetType());
+
+				return ((ColorFrame) _thisFrame).RawColorImageFormat;
+			}
+
+			public void CopyRawFrameToArray(byte[] pixels)
+			{
+				if (!(_thisFrame is ColorFrame))
+					throw new NotSupportedException("CopyRawFrameToArray() is not supported for frame of type " + _thisFrame.GetType());
+
+				ColorFrame frame = _thisFrame as ColorFrame;
+
+				if(frame.RawColorImageFormat == ColorImageFormat.Bgra)
+					frame.CopyRawFrameDataToArray(pixels);
+				else
 					frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
-				}
-
-				int stride = width * format.BitsPerPixel / 8;
-
-				visibleSource = BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
-				visiblePreview.Source = visibleSource;
-				frame.Dispose();
-
 			}
 
-		}
-
-
-		private void onDepthFrameArrived(object sender, DepthFrameArrivedEventArgs e)
-		{
-
-			ushort[] array = null;
-
-			DepthFrame frame = e.FrameReference.AcquireFrame();
-			if (frame != null)
+			public void CopyConvertedFrameDataToArray(byte[] pixels, ColorImageFormat colorImageFormat)
 			{
-				FrameDescription frameDescription = frame.FrameDescription;
+				if (!(_thisFrame is ColorFrame))
+					throw new NotSupportedException("CopyConvertedFrameDataToArray() is not supported for frame of type " + _thisFrame.GetType());
 
-				if (frameDescription != null)
-				{
-					array = new ushort[frameDescription.Width * frameDescription.Height];
-					frame.CopyFrameDataToArray(array);
-
-					// Copy the damn thing
-					byte[] arrayCopy = new byte[array.Length * 2];
-					int outIndex = 0;
-					for (int i = 0; i < array.Length; ++i)
-					{
-						arrayCopy[outIndex++] = (byte)(array[i] & 0x00ff);
-						arrayCopy[outIndex++] = (byte)((array[i] & 0xff00) >> 8);
-					}
-
-					int width = frameDescription.Width;
-					int height = frameDescription.Height;
-
-					BitmapPalette myPalette = BitmapPalettes.Gray16; //;new BitmapPalette(colors);
-
-					depthSource = BitmapSource.Create(
-						width,
-						height,
-						96,
-						96,
-						PixelFormats.Gray16,
-						myPalette,
-						arrayCopy,
-						width * 2);
-					depthPreview.Source = depthSource;
-				}
-				frame.Dispose();
+				((ColorFrame)_thisFrame).CopyConvertedFrameDataToArray(pixels, colorImageFormat);
 			}
 
-		}
-
-		private void OnInfraredArrived(object sender, InfraredFrameArrivedEventArgs infraredFrameArrivedEventArgs)
-		{
-			ushort[] array = null;
-
-			InfraredFrame frame = infraredFrameArrivedEventArgs.FrameReference.AcquireFrame();
-			if (frame != null)
+			public void Dispose()
 			{
-				FrameDescription frameDescription = frame.FrameDescription;
-
-				if (frameDescription != null)
-				{
-					array = new ushort[frameDescription.Width * frameDescription.Height];
-					frame.CopyFrameDataToArray(array);
-
-					// Copy the damn thing
-					byte[] arrayCopy = new byte[array.Length * 2];
-					int outIndex = 0;
-					for (int i = 0; i < array.Length; ++i)
-					{
-						arrayCopy[outIndex++] = (byte)(array[i] & 0x00ff);
-						arrayCopy[outIndex++] = (byte)((array[i] & 0xff00) >> 8);
-					}
-
-					int width = frameDescription.Width;
-					int height = frameDescription.Height;
-
-					BitmapPalette myPalette = BitmapPalettes.Gray16; //;new BitmapPalette(colors);
-
-					infraredSource = BitmapSource.Create(
-						width,
-						height,
-						96,
-						96,
-						PixelFormats.Gray16,
-						myPalette,
-						arrayCopy,
-						width * 2);
-					colorPreview.Source = infraredSource;
-				}
-				frame.Dispose();
+				_thisFrame.Dispose();
 			}
+
+			public void CopyFrameDataToArray(ushort[] pixels)
+			{
+				if(_thisFrame is ColorFrame )
+					throw new NotSupportedException("CopyFrameDataToArray() is not supported for frame of type " + _thisFrame.GetType());
+				else if(_thisFrame is DepthFrame)
+					(_thisFrame as DepthFrame).CopyFrameDataToArray(pixels);
+				else 
+					(_thisFrame as InfraredFrame).CopyFrameDataToArray(pixels);
+			}
+
+			public PixelFormat PixelFormat()
+			{
+				return (_thisFrame is ColorFrame) ? PixelFormats.Bgr32 : PixelFormats.Gray16  ;
+			}
+
+			public int Stride()
+			{
+				return FrameDescription().Width * ((_thisFrame is ColorFrame) ? PixelFormat().BitsPerPixel / 8 : 2);
+			}
+
+			public byte[] getPixelArray()
+			{
+				int width = FrameDescription().Width;
+				int height = FrameDescription().Height;
+
+				if (_thisFrame is ColorFrame)
+					return new byte[width * height * ((PixelFormat().BitsPerPixel + 7) / 8)];
+				else
+					return new byte[width * height * 2];
+			}
+
 		}
 
 		private void InfraredSave_Click(object sender, RoutedEventArgs e)
 		{
-			if (colorPreview.Source != null)
+			if (infraredPreview.Source != null)
 			{
-				SaveBmpFromImageSource(colorPreview.Source, "Infrared.bmp");
+				SaveBmpFromImageSource(infraredPreview.Source, "Infrared.tiff");
 			}
-		}
-
-		private void SaveBmpFromImageSource(ImageSource source, String filename)
-		{
-			Console.WriteLine(infraredSource);
-			Image outImage = new Image();
-			outImage.Width = infraredSource.Width;
-			outImage.Height = infraredSource.Height;
-			outImage.Source = infraredSource;
-
-			FileStream stream = new FileStream(filename, FileMode.Create);
-			BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-			TextBlock myTextBlock = new TextBlock();
-			myTextBlock.Text = "Codec Author is: " + encoder.CodecInfo.Author.ToString();
-			encoder.Frames.Add(BitmapFrame.Create(infraredSource));
-			encoder.Save(stream);
 		}
 
 		private void DepthSave_OnClick(object sender, RoutedEventArgs e)
 		{
 			if (depthPreview.Source != null)
 			{
-				SaveBmpFromImageSource(depthPreview.Source, "Depth.bmp");
+				SaveBmpFromImageSource(depthPreview.Source, "Depth.tiff");
 			}
 		}
 
@@ -211,9 +222,26 @@ namespace WpfApp1
 		{
 			if (colorPreview.Source != null)
 			{
-				SaveBmpFromImageSource(colorPreview.Source, "Infrared.bmp");
+				SaveBmpFromImageSource(colorPreview.Source, "Color.tiff");
 			}
 		}
+
+		private void SaveBmpFromImageSource(ImageSource source, String filename)
+		{
+			Console.WriteLine(source);
+			Image outImage = new Image();
+			outImage.Width = source.Width;
+			outImage.Height = source.Height;
+			outImage.Source = source;
+
+			FileStream stream = new FileStream(filename, FileMode.Create);
+			TiffBitmapEncoder encoder = new TiffBitmapEncoder();
+			TextBlock myTextBlock = new TextBlock();
+			myTextBlock.Text = "Codec Author is: " + encoder.CodecInfo.Author.ToString();
+			encoder.Frames.Add(BitmapFrame.Create((BitmapSource)source));
+			encoder.Save(stream);
+		}
+
 		private void Close_Click(object sender, RoutedEventArgs e)
 		{
 			sensor.Close();
