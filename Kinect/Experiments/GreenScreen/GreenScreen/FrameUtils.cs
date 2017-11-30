1934917604887b5a6a8647f4ee9ff83c9f5727a8
Michael Peterson <mikebean4551@gmail.com>
2017-11-29 17:06:44 -0700

    Refactor GreenScreen Project

---
+++ Kinect/Experiments/GreenScreen/GreenScreen/FrameUtils.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GreenScreen
+{
+	class FrameManager
+	{
+		private static FrameManager instance;
+		
+		private FrameManager(){}
+
+		public static FrameManager GetInstance()
+		{
+			if (instance == null)
+				instance = new FrameManager();
+			return instance;
+		}
+
+		//private Object colorPixelsLock = new System.Object();
+		//private Object depthPixelsLock = new Object();
+		//private Object infraredPixelsLock = new Object();
+
+		private Dictionary<SourceType, Box> frameResolutions = new Dictionary<SourceType, Box>();
+		private Dictionary<SourceType, PixelFormat> framePixelFormats = new Dictionary<SourceType, PixelFormat>();
+		
+		//private ImmutableList<FrameSourceTypes> compatableFrameSourceTypes = ImmutableList.Create<FrameSourceTypes>()
+		//	.Add(FrameSourceTypes.Color)
+		//	.Add(FrameSourceTypes.Depth)
+		//	.Add(FrameSourceTypes.Infrared);
+			
+		private byte[] colorPixels { get; set; }
+		private byte[] depthPixels { get; set; }
+		private UInt16[] infraredPixels { get; set; }
+		private int min, max;
+
+		public Boolean HaveAllFrames() { return colorPixels != null && depthPixels != null && infraredPixels != null; }
+
+		public BitmapSource buildBitmapSourceFromFrame(SourceType frameSourceType)
+		{
+			if (frameResolutions.ContainsKey(frameSourceType))
+			{
+				Box dimensions = frameResolutions[frameSourceType];
+				PixelFormat format = framePixelFormats[frameSourceType];
+
+				byte[] outPixels;
+				if (frameSourceType == SourceType.COLOR)
+				{
+					outPixels = colorPixels;
+				}
+				else if (frameSourceType == SourceType.DEPTH)
+				{
+					outPixels = depthPixels;
+				}
+				else
+				{
+					outPixels = new byte[dimensions.Area * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+					int colorIndex = 0;
+					int minDepth = frameSourceType == SourceType.DEPTH ? min : 0;
+					int maxDepth = frameSourceType == SourceType.DEPTH ? max : 0;
+					UInt16[] thisPixelArray = infraredPixels;// frameSourceType == SourceType.DEPTH ? depthPixels : infraredPixels;
+
+					for (int index = 0; index < thisPixelArray.Length; ++index)
+					{
+						UInt16 pixel = thisPixelArray[index];
+						byte intensity = (byte)((frameSourceType == SourceType.INFRARED) ? (pixel >> 8) : (pixel >= minDepth && pixel <= maxDepth) ? pixel : 0);
+
+						outPixels[colorIndex++] = intensity;
+						outPixels[colorIndex++] = intensity;
+					}
+				}
+				return BitmapSource.Create(dimensions.Width, dimensions.Height, 96, 96, format, null, outPixels, dimensions.Width * format.BitsPerPixel / 8);
+			}
+			else
+			{
+				//throw new Exception("There is no frame available for source type " + frameSourceType);
+				return null;
+			}
+		}
+
+		private void processColorFrame(ColorFrame colorFrame)
+		{
+			if (colorFrame != null)
+			{
+				Box dimensions = Box.With(colorFrame.FrameDescription.Width, colorFrame.FrameDescription.Height);
+				frameResolutions[SourceType.COLOR] = dimensions;
+				framePixelFormats[SourceType.COLOR] = PixelFormats.Bgr32;
+				
+				if (colorPixels == null)
+					colorPixels = new byte[dimensions.Area * 4];
+
+				if (colorFrame.RawColorImageFormat == ColorImageFormat.Bgra)
+					colorFrame.CopyRawFrameDataToArray(colorPixels);
+				else
+					colorFrame.CopyConvertedFrameDataToArray(colorPixels, ColorImageFormat.Bgra);
+				colorFrame?.Dispose();
+			}
+		}
+
+		private void processDepthFrame(DepthFrame depthFrame)
+		{
+			if (depthFrame != null)
+			{
+				Box dimensions = Box.With(depthFrame.FrameDescription.Width, depthFrame.FrameDescription.Height);
+				frameResolutions[SourceType.DEPTH] = Box.With(depthFrame.FrameDescription.Width, depthFrame.FrameDescription.Height);
+				framePixelFormats[SourceType.DEPTH] = PixelFormats.Gray16;
+
+				min = depthFrame.DepthMinReliableDistance;
+				max = depthFrame.DepthMaxReliableDistance;
+
+				if (depthPixels == null)
+					depthPixels = new byte[dimensions.Area * (39/8)];
+
+				UInt16[] tempPixels = new UInt16[dimensions.Area];
+
+				depthFrame.CopyFrameDataToArray(tempPixels);
+
+
+				int colorIndex = 0;
+				for (int index = 0; index < tempPixels.Length; ++index)
+				{
+					UInt16 pixel = tempPixels[index];
+					byte intensity = (byte)(pixel);/// ((pixel - min) % (max - min));//(byte)((pixel >= minDepth && pixel <= maxDepth) ? pixel : 0);
+
+					//depthPixels[colorIndex++] = intensity;
+					colorIndex++;
+					depthPixels[colorIndex++] = intensity;
+				}
+
+				depthFrame?.Dispose();
+			}
+		}
+
+		private void processInfraredFrame(InfraredFrame infraredFrame)
+		{
+			if (infraredFrame != null)
+			{
+				Box dimensions = Box.With(infraredFrame.FrameDescription.Width, infraredFrame.FrameDescription.Height);
+				frameResolutions[SourceType.INFRARED] = dimensions;
+				framePixelFormats[SourceType.INFRARED] = PixelFormats.Gray16;
+
+				if (infraredPixels == null)
+					infraredPixels = new UInt16[dimensions.Area];
+
+				infraredFrame.CopyFrameDataToArray(infraredPixels);
+
+				/*
+
+				outPixels = new byte[dimensions.Area * ((PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+				int colorIndex = 0;
+				int minDepth = frameSourceType == SourceType.DEPTH ? min : 0;
+				int maxDepth = frameSourceType == SourceType.DEPTH ? max : 0;
+				UInt16[] thisPixelArray = frameSourceType == SourceType.DEPTH ? depthPixels : infraredPixels;
+
+				for (int index = 0; index < thisPixelArray.Length; ++index)
+				{
+					UInt16 pixel = thisPixelArray[index];
+					byte intensity = (byte)((frameSourceType == SourceType.INFRARED) ? (pixel >> 8) : (pixel >= minDepth && pixel <= maxDepth) ? pixel : 0);
+
+					outPixels[colorIndex++] = intensity;
+					outPixels[colorIndex++] = intensity;
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+				*/
+				infraredFrame?.Dispose();
+			}
+		}
+
+		public Boolean ProcessMultiSourceFrameEvent(MultiSourceFrameArrivedEventArgs eventArgs)
+		{
+			processColorFrame(eventArgs.FrameReference.AcquireFrame().ColorFrameReference.AcquireFrame());
+			processDepthFrame(eventArgs.FrameReference.AcquireFrame().DepthFrameReference.AcquireFrame());
+			processInfraredFrame(eventArgs.FrameReference.AcquireFrame().InfraredFrameReference.AcquireFrame());
+
+			return HaveAllFrames();
+		}
+	}
+}
--- Kinect/Experiments/GreenScreen/GreenScreen/MainWindow.xaml.cs
+++ Kinect/Experiments/GreenScreen/GreenScreen/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Kinect;
 
 namespace GreenScreen
@@ -21,42 +12,29 @@ namespace GreenScreen
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private SourceType selectedSource;
 		private KinectSensor sensor;
-		private CoordinateMapper coordinateMapper;
+		private FrameManager frameManager;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
-
-
+		
 		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 		{
-			int width = 100;
-			int height = 100;
-			byte[] pixels = new byte[width * height * 3];
-			int outIndex = 0;
-			for (int i = 0; i < width * height; ++i)
-			{
-				pixels[outIndex++] = 0xFF;
-				pixels[outIndex++] = 0x00;
-				pixels[outIndex++] = 0x00;
-			}
-
-			MyImage.Source = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr24, null, pixels, width * 3);
-
+			frameManager = FrameManager.GetInstance();
 			sensor = KinectSensor.GetDefault();
-			if (sensor == null || !sensor.IsAvailable)
-			{
-				System.Windows.MessageBox.Show(this, "There was a problem Communicating with the Kinect Sensor",
-					"Can't connect to sensor", MessageBoxButton.OK);
-				shutdown();
-			}
-			else
-			{
-				coordinateMapper = sensor.CoordinateMapper;
-				sensor.
-			}
+			selectedSource = SourceType.COLOR;
+			sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared)
+				.MultiSourceFrameArrived += OnMultiSourceFrameArrived;
+				sensor.Open(); 
+		}
+		
+		private void OnMultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs multiSourceFrameArrivedEventArgs)
+		{
+			frameManager.ProcessMultiSourceFrameEvent(multiSourceFrameArrivedEventArgs);
+			MyImage.Source = frameManager.buildBitmapSourceFromFrame(selectedSource);
 		}
 
 		private void Exit_OnClick(object sender, RoutedEventArgs e)
@@ -69,5 +47,14 @@ namespace GreenScreen
 			sensor?.Close();
 			this.Close();
 		}
+
+		private void SourceRadioGroup_OnChecked(object sender, RoutedEventArgs e)
+		{
+			//Enum.TryParse((String) (sender as RadioButton).Content, out selectedSource);
+			RadioButton radioButton = sender as RadioButton;
+			String tagString = radioButton.Tag.ToString();
+			selectedSource = SourceType.fromString(tagString);
+		}
 	}
 }
+ 
