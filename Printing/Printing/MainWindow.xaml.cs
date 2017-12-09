7ee21eab0efc6f99405f1cf228e9876811ce6b8d
Michael Peterson <mikebean4551@gmail.com>
2017-12-09 09:53:46 -0700

    Got printing to work

--- Printing/Printing/MainWindow.xaml.cs
+++ Printing/Printing/MainWindow.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
 using System.Printing;
+using System.IO;
 
 namespace Printing
 {
@@ -38,11 +41,41 @@ namespace Printing
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			foreach (var thisPrinter in printerCollection )
+			PrintQueue targetPrinter = null;
+			foreach (PrintQueue thisPrinter in printerCollection )
 			{
-				 Console.WriteLine("{0} {1} {2} {3} {4}",thisPrinter.FullName, thisPrinter.Description, thisPrinter.DefaultPriority, thisPrinter.HasPaperProblem, thisPrinter.IsDirect);
+				//Console.WriteLine("{0} {1} {2} {3} {4}",thisPrinter.FullName, thisPrinter.Description, thisPrinter.DefaultPriority, thisPrinter.HasPaperProblem, thisPrinter.IsDirect);
+				if (thisPrinter.Name.ToLower().Contains("pdf"))
+					targetPrinter = thisPrinter;
 			}
-			
+
+			if (targetPrinter != null)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Printer:  {0}", targetPrinter.FullName);
+
+				PrintTicket ticket = targetPrinter.DefaultPrintTicket;
+				PrintCapabilities capabilities = targetPrinter.GetPrintCapabilities();
+				try
+				{
+					Visual visual = this.GetVisualChild(0);
+//					FixedPage fixedPage = new FixedPage();
+
+					//XpsDocument document = new XpsDocument("thing", FileAccess.ReadWrite);
+
+					XpsDocumentWriter writer = PrintQueue.CreateXpsDocumentWriter(targetPrinter); //XpsDocument.CreateXpsDocumentWriter(targetPrinter); //GetPrintXpsDocumentWriter(); //new XpsDocumentWriter(visual);
+					writer.Write(visual);
+					targetPrinter.Commit();
+					printServer.Commit();
+					
+					//PrintSystemJobInfo PrintJob = targetPrinter.AddJob( AddJob("new Job", ticket);
+				}
+				catch(Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+
 		}
 	}
 }
