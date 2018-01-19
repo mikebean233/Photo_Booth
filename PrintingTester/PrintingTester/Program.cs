82a16870f3f789331f4d304c782252e30d10a605
Michael Peterson <mikebean4551@gmail.com>
2018-01-18 21:44:46 -0700

    Started PrintingTester project

---
+++ PrintingTester/PrintingTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+using Printing;
+
+namespace PrintingTester
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            PrintManager printManager = PrintManager.GetInstance("pdf");
+            printManager.AddImage(GetImageSourceFromPath("Kinect_Standing.bmp"));
+            printManager.AddImage(GetImageSourceFromPath("Kinect_Leaning.bmp"));
+            Boolean result = printManager.print();
+
+            Console.WriteLine("Print result: {0}", result ? "success" : "failure");
+        }
+
+        static ImageSource GetImageSourceFromPath(String path)
+        {
+            return new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+        }
+    }
+}
