f852e904afba6b7d52125ca400271575822c0cc8
Michael Peterson <mikebean4551@gmail.com>
2018-02-24 19:53:27 -0700

    Changing Imaging project to a class library

--- Imaging/Imaging/ImageProducerFactory.cs
+++ Imaging/Imaging/ImageProducerFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Imaging
 {
-    class ImageProducerFactory
+    public class ImageProducerFactory
     {
         public static ImageProducer GetImageProducer()
         {
